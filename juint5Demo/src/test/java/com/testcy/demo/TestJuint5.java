683c0788680ef63580b79383dab154650e6db3c9
TestCurry <987899472@qq.com>
2021-05-27 08:56:29 +0800

    提交测试代码

---
+++ juint5Demo/src/test/java/com/testcy/demo/TestJuint5.java
@@ -0,0 +1,17 @@
+package com.testcy.demo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+public class TestJuint5 {
+
+    @Test
+    @DisplayName("测试执行中。。。")
+    public void test01(){
+        System.out.println("测试执行中。。。");
+        Assertions.assertTrue(true);
+    }
+
+}
