ab83dc8b45c35e06f0684d9abcd47cb978b69bb1
TestCurry <987899472@qq.com>
2021-05-27 09:25:10 +0800

    修改pom maven插件版本

--- juint5Demo/src/test/java/com/testcy/demo/TestJuint5.java
+++ juint5Demo/src/test/java/com/testcy/demo/TestJuint5.java
@@ -3,8 +3,9 @@ package com.testcy.demo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-
+@SpringBootTest
 public class TestJuint5 {
 
     @Test
